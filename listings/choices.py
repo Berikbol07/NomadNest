661aaf6d478f9bf9b6fe956eda1650973df3eaee
Berikbol07 <berikbolzholdasbaiuly@gmail.com>
2025-04-10 18:43:31 +0500

    Add files via upload

---
+++ listings/choices.py
@@ -0,0 +1,41 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+
+}
+
+price_choices = {
+    '10000000': '₸10M',
+    '20000000': '₸20M',
+    '30000000': '₸30M',
+    '40000000': '₸40M',
+    '50000000': '₸50M',
+    '60000000': '₸60M',
+    '70000000': '₸70M',
+    '80000000': '₸80M',
+    '90000000': '₸90M',
+    '1000000000': '₸100M+',
+}
+
+state_choices = {
+    "ALA": "Almaty",
+    "AST": "Astana",
+    "SHY": "Shymkent",
+    "AKT": "Aktobe",
+    "ATY": "Atyrau",
+    "KAR": "Karaganda",
+    "KOK": "Kokshetau",
+    "KYZ": "Kyzylorda",
+    "PAV": "Pavlodar",
+    "SEM": "Semey",
+    "TAR": "Taraz",
+    "TAL": "Taldykorgan",
+    "URA": "Uralsk",
+    "UST": "Oskemen",
+    "PET": "Petropavl",
+    "TEM": "Temirtau",
+}
